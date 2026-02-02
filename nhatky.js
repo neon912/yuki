672a161282349f6122b22fee4d6215b73d41d9ee
neon912 <neon69231@gmail.com>
2026-02-02 17:59:35 +0900

    Add files via upload

---
+++ nhatky.js
@@ -0,0 +1,12 @@
+const SITE_CONFIG = {
+    adminName: "Kirya Yuki",
+    siteName: "Kho lưu trữ",
+    since: "2026",
+    welcomeText: "Chào mừng đến với Kho lưu trữ...",
+    introTitle: "Giới thiệu",
+    introContent: `<p>Admin: Kirya Yuki...</p>`
+};
+
+const MY_DIARY = [
+    { date: "2026/02/02", content: "Hôm nay nắng đpẹ." }
+];
